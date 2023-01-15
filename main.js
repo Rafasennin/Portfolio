8b15f2b4748d74fdf2a9ca755fd8820a92cf0dcc
Rafael-Santos <117246770+Rafasennin@users.noreply.github.com>
2023-01-15 13:21:27 -0300

    Animation improvement and email addition.
    addition o snow man and winter time to the animation.
    addition of personal e-mail to the bottom of contact page.

--- main.js
+++ main.js
@@ -22,6 +22,119 @@ const clound8 = svg.append("circle").attr("cx",930).attr("cy", 85).attr("r", 45)
 const clound9 = svg.append("circle").attr("cx",1000).attr("cy", 65).attr("r", 60).attr("fill", "white");
 const clound10 = svg.append("circle").attr("cx",1065).attr("cy", 75).attr("r", 42).attr("fill", "white");
 
+var active = false;
+
+
+
+    const baseCircle = svg.append("circle")
+                          .attr("cx",-100)
+                          .attr("cy", 390)
+                          .attr("r", 70)
+                          .attr("fill", "snow");
+
+    const midleCircle = svg.append("circle")
+                           .attr("cx",-100)
+                           .attr("cy", 300)
+                           .attr("r", 55)
+                           .attr("fill", "snow");
+
+    const topCircle = svg.append("circle")
+                         .attr("cx",-100)
+                         .attr("cy", 220)
+                         .attr("r", 40)
+                         .attr("fill", "snow");
+
+    const leftArm = svg.append("line")
+                       .attr("x1",-100)
+                       .attr("y1",295)
+                       .attr("x2",-100)
+                       .attr("y2",270)
+                       .attr("stroke","black")
+                       .attr("stroke-width",10);
+
+    const leftFingerUp = svg.append("line")
+                            .attr("x1",-100)
+                            .attr("y1",280)
+                            .attr("x2",-100)
+                            .attr("y2",242)
+                            .attr("stroke","black")
+                            .attr("stroke-width",10);
+
+    const leftFingerDown = svg.append("line")
+                              .attr("x1",-100)
+                              .attr("y1",280)
+                              .attr("x2",-100)
+                              .attr("y2",300)
+                              .attr("stroke","black")
+                              .attr("stroke-width",10);
+
+    const rightArm = svg.append("line")
+                        .attr("x1",-100)
+                        .attr("y1",295)
+                        .attr("x2",-100)
+                        .attr("y2",270)
+                        .attr("stroke","black")
+                        .attr("stroke-width",10);
+
+    const rightFingerUp = svg.append("line")
+                             .attr("x1",-100)
+                             .attr("y1",283)
+                             .attr("x2",-100)
+                             .attr("y2",242)
+                             .attr("stroke","black")
+                             .attr("stroke-width",10);
+
+    const rightFingerDown = svg.append("line")
+                            .attr("x1",-100)
+                            .attr("y1",283)
+                            .attr("x2",-100)
+                            .attr("y2",300)
+                            .attr("stroke","black")
+                            .attr("stroke-width",10);
+
+    const upperDot = svg.append("circle")
+                        .attr("cx",-100)
+                        .attr("cy", 290)
+                        .attr("r", 10)
+                        .attr("fill", "black");
+
+    const middleDot1 = svg.append("circle")
+                        .attr("cx",-100)
+                        .attr("cy", 325)
+                        .attr("r", 10)
+                        .attr("fill", "black");
+
+    const middleDot2 = svg.append("circle")
+                        .attr("cx",-100)
+                        .attr("cy", 360)
+                        .attr("r", 10)
+                        .attr("fill", "black");
+
+    const leftEye = svg.append("circle")
+                        .attr("cx",-100)
+                        .attr("cy", 215)
+                        .attr("r", 7)
+                        .attr("fill", "black");
+
+    const righttEye = svg.append("circle")
+                        .attr("cx",-100)
+                        .attr("cy", 215)
+                        .attr("r", 7)
+                        .attr("fill", "black");
+
+    const mouth = svg.append("ellipse")
+                        .attr("ry",7)
+                        .attr("rx", 20)
+                        .attr("cy",240)
+                        .attr("cx", -100)
+                        .attr("stroke","red")
+                        .attr("stroke-width",5)
+                        .attr("fill", "black");
+    
+   
+
+
+
 
 function pickRandom(min, max){
 
@@ -29,14 +142,12 @@ function pickRandom(min, max){
     
  }
  
-var timeOfDay = ["morning", "noon", "night", "rainy"];
+var timeOfDay = ["morning", "noon", "night", "rainy", "snowy"];
 
 var switchTimeCircle = svg.append("circle").attr("cx",1300).attr("cy",60).attr("r", 40).attr("fill","darkgrey");
 var switchTimeCircle2 = svg.append("circle").attr("cx",1300).attr("cy",60).attr("r", 30).attr("fill","red");
 var text = svg.append("text").text("SWITCH").attr("x",1238).attr("y",130).attr("fill", "gold")
 .attr("font-size",30).attr("stroke","black").attr("font-family","sans-serif").attr("stroke-width",1,3);
-
-var active = false;
 
 
 function changeWeather(time){
@@ -56,6 +167,22 @@ function changeWeather(time){
         clound10.attr("fill","#FFFFFF");
 
         active = false;
+
+        topCircle.attr("cx", -100);
+        midleCircle.attr("cx", -100);
+        baseCircle.attr("cx", -100);
+        leftArm.attr("x1", -100).attr("x2", -100);
+        leftFingerUp.attr("x1", -100).attr("x2", -100);
+        leftFingerDown.attr("x1", -100).attr("x2", -100);
+        rightArm.attr("x1", -100).attr("x2", -100);
+        rightFingerUp.attr("x1", -100).attr("x2", -100);
+        rightFingerDown.attr("x1", -100).attr("x2", -100);
+        upperDot.attr("cx", -100).attr("x2", -100);
+        middleDot1.attr("cx", -100);
+        middleDot2.attr("cx", -100).attr("x2", -100);
+        leftEye.attr("cx", -100);
+        righttEye.attr("cx", -100);
+        mouth.attr("cx", -100);
     }
 
     if (time === "noon") {
@@ -73,6 +200,22 @@ function changeWeather(time){
         clound10.attr("fill","#FFFFFF"); 
 
         active = false;
+
+        topCircle.attr("cx", -100);
+        midleCircle.attr("cx", -100);
+        baseCircle.attr("cx", -100);
+        leftArm.attr("x1", -100).attr("x2", -100);
+        leftFingerUp.attr("x1", -100).attr("x2", -100);
+        leftFingerDown.attr("x1", -100).attr("x2", -100);
+        rightArm.attr("x1", -100).attr("x2", -100);
+        rightFingerUp.attr("x1", -100).attr("x2", -100);
+        rightFingerDown.attr("x1", -100).attr("x2", -100);
+        upperDot.attr("cx", -100).attr("x2", -100);
+        middleDot1.attr("cx", -100);
+        middleDot2.attr("cx", -100).attr("x2", -100);
+        leftEye.attr("cx", -100);
+        righttEye.attr("cx", -100);
+        mouth.attr("cx", -100);
     }
 
     if (time === "night") {
@@ -90,6 +233,22 @@ function changeWeather(time){
         clound10.attr("fill","#D0D0D0");
 
         active = false;
+
+        topCircle.attr("cx", -100);
+        midleCircle.attr("cx", -100);
+        baseCircle.attr("cx", -100);
+        leftArm.attr("x1", -100).attr("x2", -100);
+        leftFingerUp.attr("x1", -100).attr("x2", -100);
+        leftFingerDown.attr("x1", -100).attr("x2", -100);
+        rightArm.attr("x1", -100).attr("x2", -100);
+        rightFingerUp.attr("x1", -100).attr("x2", -100);
+        rightFingerDown.attr("x1", -100).attr("x2", -100);
+        upperDot.attr("cx", -100).attr("x2", -100);
+        middleDot1.attr("cx", -100);
+        middleDot2.attr("cx", -100).attr("x2", -100);
+        leftEye.attr("cx", -100);
+        righttEye.attr("cx", -100);
+        mouth.attr("cx", -100);
     }
 
     if(time === "rainy"){
@@ -108,14 +267,73 @@ function changeWeather(time){
         clound9.attr("fill","grey");
         clound10.attr("fill","grey");
 
-       setInterval(rainDown,700);
+       setInterval(rainDown,500);
 
        active = true;
+
+       topCircle.attr("cx", -100);
+        midleCircle.attr("cx", -100);
+        baseCircle.attr("cx", -100);
+        leftArm.attr("x1", -100).attr("x2", -100);
+        leftFingerUp.attr("x1", -100).attr("x2", -100);
+        leftFingerDown.attr("x1", -100).attr("x2", -100);
+        rightArm.attr("x1", -100).attr("x2", -100);
+        rightFingerUp.attr("x1", -100).attr("x2", -100);
+        rightFingerDown.attr("x1", -100).attr("x2", -100);
+        upperDot.attr("cx", -100).attr("x2", -100);
+        middleDot1.attr("cx", -100);
+        middleDot2.attr("cx", -100).attr("x2", -100);
+        leftEye.attr("cx", -100);
+        righttEye.attr("cx", -100);
+        mouth.attr("cx", -100);
+
+    } 
+
+    if(time === "snowy"){
+
+        sky.attr("fill","grey"); 
+        sun.attr("cx",670).attr("cy", 85).attr("r", 70).attr("fill", "grey"); 
+
+        clound1.attr("fill","snow");
+        clound2.attr("fill","snow");
+        clound3.attr("fill","snow");
+        clound4.attr("fill","snow");
+        clound5.attr("fill","snow");
+        clound6.attr("fill","snow");
+        clound7.attr("fill","snow");
+        clound8.attr("fill","snow");
+        clound9.attr("fill","snow");
+        clound10.attr("fill","snow");
+
+        active = false;
+
+        topCircle.attr("cx",975).attr("cy",1000).transition().duration(3000).ease(d3.easeQuadIn).attr("cy",220);
+        midleCircle.attr("cx",975).attr("cy",1000).transition().duration(1500).ease(d3.easeQuadIn).attr("cy",300);
+        baseCircle.attr("cx",975).attr("cy",1000).transition().duration(500).ease(d3.easeQuadIn).attr("cy",390);
+        leftArm .attr("x1",925).attr("x2",835).attr("y1",5000).attr("y2",5000).transition().duration(1500).ease(d3.easeQuadIn).attr("y1",295).attr("y2",270);
+        leftFingerUp.attr("x1",875).attr("x2",843).attr("y1",5000).attr("y2",5000).transition().duration(1500).ease(d3.easeQuadIn).attr("y1",280).attr("y2",242);
+        leftFingerDown.attr("x1",875).attr("x2",835).attr("y1",5000).attr("y2",5000).transition().duration(1500).ease(d3.easeQuadIn).attr("y1",280).attr("y2",300);
+        rightArm.attr("x1",1025).attr("x2",1115).attr("y1",5000).attr("y2",5000).transition().duration(1500).ease(d3.easeQuadIn).attr("y1",295).attr("y2",270);
+        rightFingerUp.attr("x1",1070) .attr("x2",1105).attr("y1",5000).attr("y2",5000).transition().duration(1500).ease(d3.easeQuadIn).attr("y1",283).attr("y2",242);
+        rightFingerDown.attr("x1",1070).attr("x2",1115).attr("y1",5000).attr("y2",5000).transition().duration(1500).ease(d3.easeQuadIn).attr("y1",283).attr("y2",300);
+        upperDot .attr("cx",975).attr("cy",5000).transition().duration(3000).ease(d3.easeQuadIn).attr("cy",290);
+        middleDot1 .attr("cx",975).attr("cy",5000).transition().duration(1500).ease(d3.easeQuadIn).attr("cy",325);
+        middleDot2 .attr("cx",975).attr("cy",5000).transition().duration(500).ease(d3.easeQuadIn).attr("cy",360);
+        leftEye.attr("cx",960).attr("cy",5000).transition().duration(3000).ease(d3.easeQuadIn).attr("cy",215);
+        righttEye.attr("cx",990).attr("cy",5000).transition().duration(3000).ease(d3.easeQuadIn).attr("cy",215);
+        mouth.attr("cx", 975).attr("cy",5000).transition().duration(3000).ease(d3.easeQuadIn).attr("cy",240);
+
+        setInterval(rainDown,3500);
+
+       active = true;
+        
     }
 }
 
+
+
 function switchOn(){
-    changeWeather(timeOfDay[pickRandom(0,3)]) 
+    changeWeather(timeOfDay[pickRandom(0,4)]) 
     return changeWeather;
 }
 
@@ -168,6 +386,7 @@ function rainDown(){
    }
     }   
 }
+
 
 //menu slide set up
 
